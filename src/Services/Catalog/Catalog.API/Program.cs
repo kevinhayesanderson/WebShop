6b6389444a0b4741809838e806367bf261e0273d
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2023-12-27 17:06:14 +0530

    Final Catalog.Api + mongo  + docker

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,8 @@
 
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 namespace Catalog.API
 {
     public class Program
@@ -12,15 +16,22 @@ namespace Catalog.API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+            });
+
+            builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
             }
 
             app.UseAuthorization();
