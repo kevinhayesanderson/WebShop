708474df90c9974f1f4a9e6f86f64469992b7fb4
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-01-02 13:15:49 +0530

    Added Presentation and Infrstucture layer implmentation for Ordering Service

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,13 @@
+using Ordering.API.Extensions;
 using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -14,9 +17,16 @@ namespace Ordering.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
             builder.Services.AddApplicationServices();
+            builder.Services.AddInfrastructureServices(builder.Configuration);
 
             var app = builder.Build();
+            await app.MigrateDatabase<OrderContext>(async (context, services) =>
+            {
+                var logger = services.GetService<ILogger<OrderContextSeed>>();
+                await OrderContextSeed.SeedAsync(context, logger);
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders() => new List<Order>
+            {
+                new()
+                {
+                    UserName = "swn",
+                    FirstName = "Kevin",
+                    LastName = "Anderson",
+                    EmailAddress = "kevinhayesanderson@gmail.com",
+                    AddressLine = "Chennai",
+                    Country = "India",
+                    TotalPrice = 350
+                }
+            };
+    }
+}
